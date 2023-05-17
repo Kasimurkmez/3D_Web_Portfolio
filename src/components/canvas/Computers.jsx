fee16d1ae0c05c1fde94034949309e2f49dd8978
kasimurkmez <114136293+Kasimurkmez@users.noreply.github.com>
2023-05-18 02:24:45 +0300

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -61,6 +61,8 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          autoRotate={true}
+          autoRotateSpeed={0.3}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
