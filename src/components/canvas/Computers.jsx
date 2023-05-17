8280d1bcd5001c98c4fc55c7812b4cedd023fe83
kasimurkmez <kasimurkmez47@gmail.com>
2023-05-18 01:45:29 +0300

    updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -61,8 +61,6 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-          autoRotate={true}
-          autoRotateSpeed={0.4}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
